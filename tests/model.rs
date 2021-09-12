43a3b1e4841abab3d8fcc80c83395fb4aca32f0e
Martin Toman <toman.martin@live.com>
2021-09-11 18:06:29 -0700

    Add more proptests

--- tests/model.rs
+++ tests/model.rs
@@ -1,16 +1,55 @@
 use proptest::proptest;
+use rand::thread_rng;
+use rand::seq::IteratorRandom;
 
 use sparser_bitfield::{Bitfield, Change};
 
-fn model(bit: usize) {
-  let mut bits = Bitfield::new();
-  assert_eq!(bits.set(bit), Change::Changed);
-  assert_eq!(bits.get(bit), true);
-}
-
 proptest! {
-  #[test]
-  fn single_bit(bit in 0usize..100_000_000) {
-    model(bit);
-  }
+    #[test]
+    fn single_bit(bit in 0usize..100_000_000) {
+        let mut bits = Bitfield::new();
+        assert_eq!(bits.set(bit), Change::Changed);
+        assert_eq!(bits.get(bit), true);
+        assert_eq!(bits.set(bit), Change::Unchanged);
+        assert_eq!(bits.get(bit), true);
+        assert_eq!(bits.set(bit), Change::Unchanged);
+        assert_eq!(bits.get(bit), true);
+    }
+
+    #[test]
+    fn two_bits(a in 0usize..10, b in 0usize..10) {
+        let mut bits = Bitfield::new();
+
+        assert_eq!(bits.set(a), Change::Changed);
+        assert_eq!(bits.get(a), true);
+
+        let change = bits.set(b);
+        let expected_change = if a == b {
+            Change::Unchanged
+        }
+        else {
+            Change::Changed
+        };
+        assert_eq!(expected_change, change);
+
+        assert_eq!(bits.get(b), true);
+    }
+
+    #[test]
+    fn utilization(load in 0usize..100) {
+        let size = 1_000;
+
+        let mut rng = thread_rng();
+        let count = (size / 100) * load;
+        let sample = (0..size).choose_multiple(&mut rng, count);
+
+        let mut bits = Bitfield::new();
+        for i in sample.clone() {
+            bits.set(i);
+        }
+
+        for i in sample {
+            assert_eq!(bits.get(i), true);
+        }
+    }
 }
